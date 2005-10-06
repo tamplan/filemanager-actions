f6dd6d637ec0d8dc0ee5d1731af578a8735490d3
Rodrigo Moya <rodrigo@novell.com>
2005-10-06 15:35:09 +0000

    base the class in NautilusActionsConfig, not GObject.
    2005-10-06  Rodrigo Moya <rodrigo@novell.com>
    
    	* libnautilus-actions/nautilus-actions-config-gconf.c
    	(nautilus_actions_config_gconf_get_type): base the class in
    	NautilusActionsConfig, not GObject.

--- libnautilus-actions/nautilus-actions-config-gconf.c
+++ libnautilus-actions/nautilus-actions-config-gconf.c
@@ -290,7 +290,7 @@ nautilus_actions_config_gconf_get_type (void)
 					0,
 					(GInstanceInitFunc) nautilus_actions_config_gconf_init
 		};
-		type = g_type_register_static (G_TYPE_OBJECT, "NautilusActionsConfigGconf", &info, 0);
+		type = g_type_register_static (NAUTILUS_ACTIONS_TYPE_CONFIG, "NautilusActionsConfigGconf", &info, 0);
 	}
 	return type;
 }
