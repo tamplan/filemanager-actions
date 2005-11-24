f6f206142a103ea52934b043d68215d93c2cddcf
Frederic Ruaudel <grumz@grumz.net>
2005-11-24 09:01:59 +0000

    Remove the old config file installation
    2005-11-24  Frederic Ruaudel <grumz@grumz.net>
    
            * config/config_newaction.xml.in:
            * config/Makefile.am: Remove the old config file installation
    
            * autogen.sh:
            * configure.in:
            * config/Makefile.am: Add support for GConf schema installation
    
            * config/config_newaction.schemas.in.in: Set the uuid to a non-random
            string so it can be recognizable.

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="nautilus-actions"
+REQUIRED_AUTOMAKE_VERSION=1.7
 
 (test -f $srcdir/configure.in ) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
