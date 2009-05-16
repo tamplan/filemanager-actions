3985d436d99e571a2e13f70ecb920e52b9cd4c19
Pierre Wieser <pwieser@trychlos.org>
2009-05-16 18:03:27 +0200

    Remove old automake dependancy from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -5,17 +5,17 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="nautilus-actions"
-REQUIRED_AUTOMAKE_VERSION=1.7
 REQUIRED_INTLTOOL_VERSION=0.35.5
 
-(test -f $srcdir/configure.in ) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
+( test -f $srcdir/configure.ac ) || {
+	echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+	echo " top-level $PKG_NAME directory"
+	exit 1
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
+	echo "You need to install gnome-common from the GNOME Git"
+	exit 1
 }
+
 USE_GNOME2_MACROS=1 . gnome-autogen.sh
