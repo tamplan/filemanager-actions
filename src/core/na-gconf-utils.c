43363a4b510a4f39c0b2f653619338a99d0c274a
Pierre Wieser <pwieser@trychlos.org>
2010-02-15 17:23:53 +0100

    Refactoring: rename src/core/na-gconf-utils.h to src/api/na-gconf-utils.h

--- src/core/na-gconf-utils.c
+++ src/core/na-gconf-utils.c
@@ -34,11 +34,91 @@
 
 #include <string.h>
 
-#include "na-utils.h"
-#include "na-gconf-utils.h"
+#include <api/na-core-utils.h>
+#include <api/na-gconf-utils.h>
 
 static GConfValue *read_value( GConfClient *gconf, const gchar *path, gboolean use_schema, GConfValueType type );
 static gboolean    sync_gconf( GConfClient *gconf, gchar **message );
+
+/**
+ * na_gconf_utils_get_subdirs:
+ * @gconf: a  #GConfClient instance.
+ * @path: a full path to be readen.
+ *
+ * Returns: a list of full path subdirectories.
+ *
+ * The returned list should be #na_gconf_utils_free_subdirs() by the caller.
+ */
+GSList *
+na_gconf_utils_get_subdirs( GConfClient *gconf, const gchar *path )
+{
+	static const gchar *thisfn = "na_gconf_utils_get_subdirs";
+	GError *error = NULL;
+	GSList *list_subdirs;
+
+	list_subdirs = gconf_client_all_dirs( gconf, path, &error );
+
+	if( error ){
+		g_warning( "%s: path=%s, error=%s", thisfn, path, error->message );
+		g_error_free( error );
+		return(( GSList * ) NULL );
+	}
+
+	return( list_subdirs );
+}
+
+/**
+ * na_gconf_utils_free_subdirs:
+ * @subdirs: the subdirectory list as returned from #na_gconf_utils_get_subdirs().
+ *
+ * Release the list.
+ */
+void
+na_gconf_utils_free_subdirs( GSList *subdirs )
+{
+	na_core_utils_slist_free( subdirs );
+}
+
+/**
+ * na_gconf_utils_has_entry:
+ * @gconf: a  #GConfClient instance.
+ * @path: the full path of a key.
+ * @entry: the entry to be tested.
+ *
+ * Returns: %TRUE if the given @entry exists for the given @path,
+ * %FALSE else.
+ */
+gboolean
+na_gconf_utils_has_entry( GConfClient *gconf, const gchar *path, const gchar *entry )
+{
+	static const gchar *thisfn = "na_gconf_utils_has_entry";
+	gboolean have_entry = FALSE;
+	GError *error = NULL;
+	gchar *key;
+	GConfValue *value;
+
+	key = g_strdup_printf( "%s/%s", path, entry );
+
+	value = gconf_client_get_without_default( gconf, key, &error );
+
+	if( error ){
+		g_warning( "%s: key=%s, error=%s", thisfn, key, error->message );
+		g_error_free( error );
+		if( value ){
+			gconf_value_free( value );
+			value = NULL;
+		}
+	}
+
+	if( value ){
+		have_entry = TRUE;
+		gconf_value_free( value );
+	}
+
+	g_free( key );
+
+	return( have_entry );
+}
 
 /**
  * na_gconf_utils_get_entries:
@@ -72,19 +152,6 @@ na_gconf_utils_get_entries( GConfClient *gconf, const gchar *path )
 }
 
 /**
- * na_gconf_utils_free_entries:
- * @list: a list of #GConfEntry as returned by na_gconf_utils_get_entries().
- *
- * Releases the provided list.
- */
-void
-na_gconf_utils_free_entries( GSList *list )
-{
-	g_slist_foreach( list, ( GFunc ) gconf_entry_unref, NULL );
-	g_slist_free( list );
-}
-
-/**
  * na_gconf_utils_get_bool_from_entries:
  * @entries: a list of #GConfEntry as returned by na_gconf_utils_get_entries().
  * @entry: the searched entry.
@@ -111,7 +178,7 @@ na_gconf_utils_get_bool_from_entries( GSList *entries, const gchar *entry, gbool
 
 	for( ip = entries ; ip && !found ; ip = ip->next ){
 		gconf_entry = ( GConfEntry * ) ip->data;
-		key = na_gconf_utils_path_to_key( gconf_entry_get_key( gconf_entry ));
+		key = g_path_get_basename( gconf_entry_get_key( gconf_entry ));
 
 		if( !strcmp( key, entry )){
 			gconf_value = gconf_entry_get_value( gconf_entry );
@@ -158,7 +225,7 @@ na_gconf_utils_get_string_from_entries( GSList *entries, const gchar *entry, gch
 
 	for( ip = entries ; ip && !found ; ip = ip->next ){
 		gconf_entry = ( GConfEntry * ) ip->data;
-		key = na_gconf_utils_path_to_key( gconf_entry_get_key( gconf_entry ));
+		key = g_path_get_basename( gconf_entry_get_key( gconf_entry ));
 
 		if( !strcmp( key, entry )){
 			gconf_value = gconf_entry_get_value( gconf_entry );
@@ -207,7 +274,7 @@ na_gconf_utils_get_string_list_from_entries( GSList *entries, const gchar *entry
 
 	for( ip = entries ; ip && !found ; ip = ip->next ){
 		gconf_entry = ( GConfEntry * ) ip->data;
-		key = na_gconf_utils_path_to_key( gconf_entry_get_key( gconf_entry ));
+		key = g_path_get_basename( gconf_entry_get_key( gconf_entry ));
 
 		if( !strcmp( key, entry )){
 			gconf_value = gconf_entry_get_value( gconf_entry );
@@ -229,56 +296,81 @@ na_gconf_utils_get_string_list_from_entries( GSList *entries, const gchar *entry
 }
 
 /**
- * na_gconf_utils_get_subdirs:
- * @gconf: a  #GConfClient instance.
- * @path: a full path to be readen.
+ * na_gconf_utils_free_entries:
+ * @list: a list of #GConfEntry as returned by na_gconf_utils_get_entries().
  *
- * Returns: a list of full path subdirectories.
- *
- * The returned list should be na_gconf_utils_free_subdirs() by the caller.
+ * Releases the provided list.
  */
-GSList *
-na_gconf_utils_get_subdirs( GConfClient *gconf, const gchar *path )
+void
+na_gconf_utils_free_entries( GSList *list )
 {
-	static const gchar *thisfn = "na_gconf_utils_get_subdirs";
-	GError *error = NULL;
-	GSList *list_subdirs;
+	g_slist_foreach( list, ( GFunc ) gconf_entry_unref, NULL );
+	g_slist_free( list );
+}
 
-	list_subdirs = gconf_client_all_dirs( gconf, path, &error );
+/**
+ * na_gconf_utils_read_string:
+ * @gconf: a #GConfClient instance.
+ * @path: the full path to the key.
+ * @use_schema: whether to use the default value from schema, or not.
+ * @default_value: default value to be used if schema is not used or
+ * doesn't exist.
+ *
+ * Returns: the required string value in a newly allocated string which
+ * should be g_free() by the caller.
+ */
+gchar *
+na_gconf_utils_read_string( GConfClient *gconf, const gchar *path, gboolean use_schema, const gchar *default_value )
+{
+	GConfValue *value = NULL;
+	gchar *result;
+
+	g_return_val_if_fail( GCONF_IS_CLIENT( gconf ), NULL );
+
+	result = g_strdup( default_value );
+
+	value = read_value( gconf, path, use_schema, GCONF_VALUE_STRING );
+
+	if( value ){
+		g_free( result );
+		result = g_strdup( gconf_value_get_string( value ));
+		gconf_value_free( value );
+	}
+
+	return( result );
+}
+
+static GConfValue *
+read_value( GConfClient *gconf, const gchar *path, gboolean use_schema, GConfValueType type )
+{
+	static const gchar *thisfn = "na_gconf_utils_read_value";
+	GError *error = NULL;
+	GConfValue *value = NULL;
+
+	if( use_schema ){
+		value = gconf_client_get( gconf, path, &error );
+	} else {
+		value = gconf_client_get_without_default( gconf, path, &error );
+	}
 
 	if( error ){
 		g_warning( "%s: path=%s, error=%s", thisfn, path, error->message );
 		g_error_free( error );
-		return(( GSList * ) NULL );
+		if( value ){
+			gconf_value_free( value );
+			value = NULL;
+		}
 	}
 
-	return( list_subdirs );
-}
+	if( value ){
+		if( value->type != type ){
+			g_warning( "%s: path=%s, found type '%u' while waiting for type '%u'", thisfn, path, value->type, type );
+			gconf_value_free( value );
+			value = NULL;
+		}
+	}
 
-/**
- * na_gconf_utils_free_subdirs:
- * @subdirs: the subdirectory list as returned from #na_gconf_utils_get_subdirs().
- *
- * Release the list.
- */
-void
-na_gconf_utils_free_subdirs( GSList *subdirs )
-{
-	na_utils_free_string_list( subdirs );
-}
-
-/**
- * na_gconf_utils_path_to_key:
- * @path: the full path of a key.
- *
- * Returns: the key itself, i.e. the last part of the @path.
- *
- * The returned string should be g_free() by the caller.
- */
-gchar *
-na_gconf_utils_path_to_key( const gchar *path )
-{
-	return( na_utils_path_extract_last_dir( path ));
+	return( value );
 }
 
 /**
@@ -338,38 +430,6 @@ na_gconf_utils_read_int( GConfClient *gconf, const gchar *path, gboolean use_sch
 	}
 
 	return( ret );
-}
-
-/**
- * na_gconf_utils_read_string:
- * @gconf: a #GConfClient instance.
- * @path: the full path to the key.
- * @use_schema: whether to use the default value from schema, or not.
- * @default_value: default value to be used if schema is not used or
- * doesn't exist.
- *
- * Returns: the required string value in a newly allocated string which
- * should be g_free() by the caller.
- */
-gchar *
-na_gconf_utils_read_string( GConfClient *gconf, const gchar *path, gboolean use_schema, const gchar *default_value )
-{
-	GConfValue *value = NULL;
-	gchar *result;
-
-	g_return_val_if_fail( GCONF_IS_CLIENT( gconf ), NULL );
-
-	result = g_strdup( default_value );
-
-	value = read_value( gconf, path, use_schema, GCONF_VALUE_STRING );
-
-	if( value ){
-		g_free( result );
-		result = g_strdup( gconf_value_get_string( value ));
-		gconf_value_free( value );
-	}
-
-	return( result );
 }
 
 /**
@@ -578,39 +638,6 @@ na_gconf_utils_remove_entry( GConfClient *gconf, const gchar *path, gchar **mess
 	}
 
 	return( ret );
-}
-
-static GConfValue *
-read_value( GConfClient *gconf, const gchar *path, gboolean use_schema, GConfValueType type )
-{
-	static const gchar *thisfn = "na_gconf_utils_read_value";
-	GError *error = NULL;
-	GConfValue *value = NULL;
-
-	if( use_schema ){
-		value = gconf_client_get( gconf, path, &error );
-	} else {
-		value = gconf_client_get_without_default( gconf, path, &error );
-	}
-
-	if( error ){
-		g_warning( "%s: path=%s, error=%s", thisfn, path, error->message );
-		g_error_free( error );
-		if( value ){
-			gconf_value_free( value );
-			value = NULL;
-		}
-	}
-
-	if( value ){
-		if( value->type != type ){
-			g_warning( "%s: path=%s, found type '%u' while waiting for type '%u'", thisfn, path, value->type, type );
-			gconf_value_free( value );
-			value = NULL;
-		}
-	}
-
-	return( value );
 }
 
 static gboolean
