41ab8575bfe812e7127078e42c94708f65adbae0
Pierre Wieser <pwieser@trychlos.org>
2010-11-22 22:01:11 +0100

    clean up aborted make distcheck before running autogen
    Rationale: gnome-autogen.sh recursively runs anywhere where it found a configure.ac or configure.in.
    As an aborted 'make distcheck' leaves the build directory, gnome-autogen.sh tries to run in it,
    and fails.

--- run-autogen.sh
+++ run-autogen.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# a nautilus-actions-x.y may remain after an aborted make distcheck
+# such a directory breaks gnome-autogen.sh generation
+# so clean it here
+find . -maxdepth 1 -type d -name 'nautilus-actions-*' | xargs rm -fr
+
 exec ./autogen.sh \
 	--prefix=$(pwd)/install \
 	--with-nautilus-extdir=$(pwd)/install/lib/nautilus \
