d647573bc9b7232939856380da43871653150b31
Pierre Wieser <pwieser@trychlos.org>
2011-01-06 18:47:18 +0100

    console_cmdline_get_description: insert a newline in the too long message

--- src/utils/console-utils.c
+++ src/utils/console-utils.c
@@ -51,8 +51,8 @@ static GLogFunc st_default_log_func = NULL;
 gchar *
 console_cmdline_get_description( void ){
 	return( g_strdup_printf( "%s.\n%s", PACKAGE_STRING,
-			_( "Bug reports are welcomed at https://bugzilla.gnome.org/enter_bug.cgi?product=nautilus-actions,"
-				" or you may prefer to mail to <maintainer@nautilus-actions.org>.\n" )));
+			_( "Bug reports are welcomed at https://bugzilla.gnome.org/enter_bug.cgi?product=nautilus-actions,\n"
+				"or you may prefer to mail to <maintainer@nautilus-actions.org>.\n" )));
 }
 
 /**
