54c29d8a51166f3b1a5efcee7874020db228d071
Pierre Wieser <pwieser@trychlos.org>
2012-01-25 21:57:37 +0100

    run-distcheck.sh: remove remaining part of an aborded build

--- run-distcheck.sh
+++ run-distcheck.sh
@@ -3,6 +3,14 @@
 srcdir=$(cd ${0%/*}; pwd)
 builddir="_build"
 
+# a nautilus-actions-x.y may remain after an aborted make distcheck
+# such a directory breaks gnome-autogen.sh generation
+# so clean it here
+for d in $(find ${srcdir} -maxdepth 2 -type d -name 'nautilus-actions-*'); do
+	chmod -R u+w $d
+	rm -fr $d
+done
+
 rm -fr ${builddir}
 rm -fr _install
 find ${srcdir}/docs/nact -type f -name '*.html' -o -name '*.pdf' | xargs rm -f
