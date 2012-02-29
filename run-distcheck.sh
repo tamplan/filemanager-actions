20cfec1a3ed3eaa9a6722394e01c9a0b2cf6db6e
Pierre Wieser <pwieser@trychlos.org>
2012-03-01 00:13:01 +0100

    run-distcheck.sh: exec desktop-file-validate

--- run-distcheck.sh
+++ run-distcheck.sh
@@ -2,6 +2,7 @@
 
 top_srcdir=$(cd ${0%/*}; pwd)
 builddir="${top_srcdir}/_build"
+installdir="${top_srcdir}/_install"
 
 # a nautilus-actions-x.y may remain after an aborted make distcheck
 # such a directory breaks gnome-autogen.sh generation
@@ -12,11 +13,12 @@ for d in $(find ${top_srcdir} -maxdepth 2 -type d -name 'nautilus-actions-*'); d
 done
 
 rm -fr ${builddir}
-rm -fr _install
+rm -fr ${installdir}
 find ${top_srcdir}/docs/nact -type f -name '*.html' -o -name '*.pdf' | xargs rm -f
 find ${top_srcdir}/docs/nact \( -type d -o -type l \) -name 'stylesheet-images' -o -name 'admon' | xargs rm -fr
 
 target=doc ${top_srcdir}/run-autogen.sh &&
 	${top_srcdir}/tools/check-po.sh -nodummy &&
 	${top_srcdir}/tools/check-headers.sh -nodummy -builddir=${builddir} &&
+	desktop-file-validate ${installdir}/share/applications/nact.desktop &&
 	make -C ${builddir} distcheck
